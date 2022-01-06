6fe34c41f370071f7aa7bb03ac1151fdfc8d4a69
Ahmed Khanzada <me@enzu.ru>
2022-01-06 18:44:11 -0500

    Add new nongnu channel

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -7,3 +7,14 @@
           (openpgp-fingerprint
            "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
        %default-channels)
+
+(cons* (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        ;; Enable signature verification:
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       %default-channels)
