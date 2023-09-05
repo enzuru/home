783b70c41ffaa3a0ff100150043945d62a8d7daa
Ahmed Khanzada <me@enzu.ru>
2023-09-05 19:13:05 -0400

    Some Pianobar helper stuff

---
+++ .config/pianobar/event_command.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from os.path import expanduser, join
+
+path = os.environ.get('XDG_CONFIG_HOME')
+if not path:
+    path = expanduser("~/.config")
+else:
+    path = expanduser(path)
+fn = join(path, 'pianobar', 'nowplaying')
+
+lines = sys.stdin.readlines()
+cmd = sys.argv[1]
+
+if cmd == 'songstart':
+    with open(fn, 'w') as f:
+        title = None
+        artist = None
+        for line in lines:
+            if "title" in line:
+                split = line.split("=")
+                title = split[1]
+            elif "artist" in line:
+                split = line.split("=")
+                artist = split[1]
+        if title and artist:
+            nowplaying = "{} by {}".format(title, artist).replace("\n", "")
+            f.write(nowplaying)
