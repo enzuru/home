b7dd0efb05974271744e2ec2a1d3cc0e4a96fd94
Ahmed Khanzada <me@enzu.ru>
2023-09-10 19:29:04 -0400

    Upgrading packages to fix Lispy

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,20 +1,17 @@
 ;; put this into profiles.clj in ~/.lein folder
 {:user  {:jvm-opts ^:replace ["-Xmx6G" "-XX:-OmitStackTraceInFastThrow"]
          :repl-options {:timeout 180000}
-         :plugins [[cider/cider-nrepl "0.37.0"]
+         :plugins [[cider/cider-nrepl "0.37.1"]
                    [refactor-nrepl "3.9.0"]
                    [lein-ancient "1.0.0-RC3"]
-                   [jonase/eastwood "0.3.14"]
-                   [lein-try "0.4.3"]
-                   [lein-cloverage "1.0.13"]
-                   [lein-count "1.0.9"]
-                   [lein-nvd "1.3.1"]]
+                   [jonase/eastwood "1.4.0"]
+                   [lein-cloverage "1.2.4"]]
          :middleware [cider-nrepl.plugin/middleware
                       refactor-nrepl.plugin/middleware]
-         :dependencies [[nrepl "0.9.0"]
-                        [fipp "0.6.22"]
-                        [criterium "0.4.5"]
-                        [org.clojure/tools.namespace "0.2.10"]]
-
+         :dependencies [[nrepl "1.0.0"]
+                        [fipp "0.6.26"]
+                        [criterium "0.4.6"]
+                        [org.clojure/tools.namespace "1.4.4"]
+                        [org.clojure/java.classpath "1.0.0"]]
          ,,,}}
 @nxvipin
