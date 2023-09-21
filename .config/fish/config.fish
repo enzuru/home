cd9ea68febe2dfe9058c309f31a617e8c8f1dc7d
Ahmed Khanzada <me@enzu.ru>
2023-09-21 17:34:53 -0400

    Improve Midnight Commander support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 set -Ux JAVA18_PACKAGE "openjdk@18"
 #set -Ux JAVA18_PATH (guix build {$JAVA18_PACKAGE} | grep -- '-jdk$')
-alias e "emacsclient -c -nw"
+alias e ~/bin/e
+set -gx EDITOR ~/bin/e
+set -gx VIEWER ~/bin/e
