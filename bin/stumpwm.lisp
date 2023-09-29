e8291451a938d569aa505c2bc9aba07fa19b4006
Ahmed Khanzada <me@enzu.ru>
2023-09-29 15:36:45 -0400

    Fix StumpWM

--- bin/stumpwm.lisp
+++ bin/stumpwm.lisp
@@ -1,7 +1,7 @@
 (ql:quickload :stumpwm)
 (ql:quickload :slynk)
 
-(slynk:create-server :dont-close t)
+(slynk:create-server :dont-close t :port 2000)
 
 (stumpwm:add-to-load-path "~/.stumpwm.d/modules")
 (in-package :stumpwm)
