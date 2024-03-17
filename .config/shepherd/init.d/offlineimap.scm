2e9baddf1774a57db17105f2157f2bd7c621cbf6
Ahmed Khanzada <me@enzu.ru>
2024-03-17 12:27:17 -0400

    Improved Midnight Commander theme

--- .config/shepherd/init.d/offlineimap.scm
+++ .config/shepherd/init.d/offlineimap.scm
@@ -1,10 +1,11 @@
-(use-modules (guix scripts environment))
+(use-modules (guix scripts environment)
+             (gnu packages))
 
 (define offlineimap
   (make <service>
     #:provides '(offlineimap)
     #:docstring "Run offlineimap"
-    #:start (make-forkexec-constructor (list "/gnu/store/44s0dzbph80crxw9q441mzmw9xf6pa5z-profile/bin/offlineimap") #:user "enzuru" #:log-file "/home/enzuru/offlineimap.log")
+    #:start (make-forkexec-constructor (list "/gnu/store/4w5hi8ygf179m89ll0cha1ydq7r9p1h1-profile/bin/offlineimap") #:user "enzuru" #:log-file "/home/enzuru/offlineimap.log")
     #:auto-start? #t
     #:respawn? #t))
 
